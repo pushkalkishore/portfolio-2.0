8aa9482ead0a31a01e185063c20cd5078d6bcf6f
Pushkal Kishore <kpushkal122@gmail.com>
2022-06-05 23:09:41 +0530

    Finished the about me section

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./ContactMe.css";
 
 function ContactMe(props) {
-    return (
-        <div>
-           hello world 
-        </div>
-    );
+  return (
+    <section className="cta">
+      <h3>
+        Have a new project in mind?<br></br> Let's collaborate and build
+        something awesome.<br></br> Let's turn that idea to an even greater
+        product 🙂😇
+      </h3>
+      <a href="#" className="hero-btn">
+        Contact Me
+      </a>
+    </section>
+  );
 }
 
 export default ContactMe;
