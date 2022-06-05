3b8ba0de45b864c4a8ed0950d2402b26cfa489ce
Pushkal Kishore <kpushkal122@gmail.com>
2022-06-05 15:24:26 +0530

    Added the about me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import "./AboutMe.css"
+import React from "react";
+import "./AboutMe.css";
+import myphoto from "../images/MyPhoto.png";
 
 function AboutMe() {
-    return (
-        <section className='aboutme'>
-           <h1>AboutMe</h1>
-           <p>Hi, I’m Pushkal Kishore. I am currently in my IIIrd year pursuing Electrical And Electronics Engineering at BIT Mesra. My interests lie in programming, web development and problem solving.</p>
-           <p>P.S. I am a Marvel Fan</p>
-        </section>
-    );
+  return (
+    <section className="aboutme">
+      <div className="a-left">
+
+          <div className="bg-image"></div>
+
+
+          <div className="a-card">
+            <img src={myphoto} alt="photo" className="a-image" />
+          </div>
+      </div>
+
+      <div className="a-right">
+        <h1>AboutMe</h1>
+        <p>
+          Hi, I’m Pushkal Kishore.<br></br> I am currently in my IIIrd year
+          pursuing Electrical And Electronics Engineering at BIT Mesra.<br></br>{" "}
+          My interests lie in programming, web development and problem solving.
+        </p>
+        <p>P.S. I am a Marvel Fan</p>
+      </div>
+    </section>
+  );
 }
 
 export default AboutMe;
