1f5652ba951566bf90162ccb75bcd19e3d09923b
Pushkal Kishore <kpushkal122@gmail.com>
2022-06-05 12:07:36 +0530

    About me section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './components/Home/Home';
+import AboutMe from './components/AboutMe/AboutMe';
 
 function App(props) {
   return (
     <div>
       <Home />
+      <AboutMe />
     </div>
   );
 }
---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./AboutMe.css"
+
+function AboutMe() {
+    return (
+        <section className='aboutme'>
+           <h1>AboutMe</h1>
+           <p>Hi, I’m Pushkal Kishore. I am currently in my IIIrd year pursuing Electrical And Electronics Engineering at BIT Mesra. My interests lie in programming, web development and problem solving.</p>
+           <p>P.S. I am a Marvel Fan</p>
+        </section>
+    );
+}
+
+export default AboutMe;
