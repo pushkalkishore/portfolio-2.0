ffa7bef806a36599e11f76fb56c2be09a8d3ef4f
Pushkal Kishore <kpushkal122@gmail.com>
2022-06-05 20:23:44 +0530

    Changed the font

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -17,12 +17,13 @@ function AboutMe() {
 
       <div className="a-right">
         <h1>AboutMe</h1>
+        <div class="line-1"></div>
         <p>
           Hi, I’m Pushkal Kishore.<br></br> I am currently in my IIIrd year
           pursuing Electrical And Electronics Engineering at BIT Mesra.<br></br>{" "}
           My interests lie in programming, web development and problem solving.
         </p>
-        <p>P.S. I am a Marvel Fan</p>
+        <p className="marvel">P.S. I am a Marvel Fan</p>
       </div>
     </section>
   );
