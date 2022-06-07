1ee59353937d05bd031c93c7423286caa9c36f35
Pushkal Kishore <kpushkal122@gmail.com>
2022-06-07 14:30:25 +0530

    Added the project photos and added their links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import "./Projects.css";
 import london from "../images/london.png"
+import p1 from "../images/p1.png"
+import p2 from "../images/p2.png"
+import p3 from "../images/p3.jpg"
+import p4 from "../images/p4.png"
+
 
 function Projects(props) {
     return (
@@ -11,27 +16,46 @@ function Projects(props) {
            <p> Check out what I have worked on.</p>
 
            <div className='row'>
+
                <div className='project-col'>
-                   <img src={london} />
+                   <img src={p1} />
                    <div className='layer'>
-                       <h3>Upastithi</h3>
+                       <a href='https://github.com/pushkalkishore/Attendance-System' target="_blank" >
+                        <h3>Upastithi</h3>
+                        </a>
                    </div>
                </div>
 
                <div className='project-col'>
-                   <img src={london} />
+                   <img src={p2} />
                    <div className='layer'>
-                       <h3>Classic Weather Application</h3>
-                   </div>
-               </div>
-
-               <div className='project-col'>
-                   <img src={london} />
-                   <div className='layer'>
-                       <h3>Previous Portfolio Website</h3>
+                   <a href='https://github.com/pushkalkishore/portfolio-2.0' target="_blank" >
+                        <h3>This Portfolio website</h3>
+                        </a>
                    </div>
                </div>
            </div>
+
+           <div className='row'>
+           <div className='project-col'>
+                   <img src={p3} />
+                   <div className='layer'>
+                   <a href='https://github.com/pushkalkishore/Classic-Weather-App' target="_blank" >
+                        <h3>Classic Weather App</h3>
+                        </a>
+                   </div>
+               </div>
+
+               <div className='project-col'>
+                   <img src={p4} />
+                   <div className='layer'>
+                   <a href='https://github.com/pushkalkishore/personal-portfolio-website-2022' target="_blank" >
+                        <h3>Previous Portfolio website</h3>
+                        </a>
+                   </div>
+               </div>
+            </div>
+
         </div>
     );
 }
